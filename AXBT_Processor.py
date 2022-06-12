8d9bc7994c117040b545d7ab47b0607b552c29d4
Casey Densmore <cdensmore101@gmail.com>
2022-06-12 18:01:33 -0400

    README fix

--- AXBT_Processor.py
+++ AXBT_Processor.py
@@ -53,6 +53,7 @@ class AXBT_Processor:
         
         self.timerange = timerange
         
+        
         #FFT thresholds
         self.fftwindow = fftwindow
         self.minfftratio = minfftratio
@@ -75,6 +76,11 @@ class AXBT_Processor:
         self.audiofile = audiofile
         
         
+        self.deadfreq = 8000
+        self.minsiglev = 1.3
+        self.triggersiglev = 1.6
+        
+        
         
         
         
@@ -91,6 +97,8 @@ class AXBT_Processor:
         self.good_f_ind = np.all((np.greater_equal(self.f, self.flims[0]), np.less_equal(self.f, self.flims[1])), axis=0)
         
         self.good_f = self.f[self.good_f_ind]
+        
+        self.dead_freq_ind = np.argmin(np.abs(self.f - self.deadfreq))
         
         
         
@@ -119,10 +127,14 @@ class AXBT_Processor:
         fp = self.good_f[maxind] 
         
         #maximum signal strength in band
-        Sp = 10*np.log10(fftdata_inrange[maxind])
+        # Sp = 10*np.log10(fftdata_inrange[maxind])
     
         #ratio of maximum signal in band to max signal total (SNR)
         Rp = fftdata_inrange[maxind]/np.max(fftdata) 
+        
+        #Signal level normalized by power at quiet frequency
+        pdead = fftdata[self.dead_freq_ind]
+        Sp = np.log10(fftdata_inrange[maxind]/pdead)
             
         return fp, Sp, Rp
         
